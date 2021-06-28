602a2657c710500b02d1973d4bdd0e5f27347dfc
Ribonney <57567251+Ribonney@users.noreply.github.com>
2021-06-28 17:50:27 +0300

    Create pm_permit_sql.py

---
+++ UserBot/modules/sql_helpers/pm_permit_sql.py
@@ -0,0 +1,39 @@
+try:
+    from UserBot.modules.sql_helpers import BASE, SESSION
+except ImportError:
+    raise AttributeError
+
+from sqlalchemy import Column, String
+
+
+class PMPermit(BASE):
+    __tablename__ = "pmpermit"
+    chat_id = Column(String(14), primary_key=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = str(chat_id)  # ensure string
+
+
+PMPermit.__table__.create(checkfirst=True)
+
+
+def is_approved(chat_id):
+    try:
+        return SESSION.query(PMPermit).filter(PMPermit.chat_id == str(chat_id)).one()
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
+
+
+def approve(chat_id):
+    adder = PMPermit(str(chat_id))
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def dissaprove(chat_id):
+    remove = SESSION.query(PMPermit).get(str(chat_id))
+    if remove:
+        SESSION.delete(remove)
+        SESSION.commit()
