b585b702c834c3e056bed5417704909cdd9cabdd
Ribonney <57567251+Ribonney@users.noreply.github.com>
2021-06-28 14:22:36 +0300

    Update __main__.py

--- UserBot/__main__.py
+++ UserBot/__main__.py
@@ -1,8 +1,8 @@
 # (C) 2021 Jayant Kageri
 
 from pyrogram import idle, Client, filters
-from _pyrogram import app, LOGGER
-from _pyrogram.modules import *
+from UserBot import app, LOGGER
+from UserBot.modules import *
 import os
 
 print("Pyrogram User Client Started \nPowered By @TheCodents\n(C) 2021 Jayant Kageri")
