47ba22d88277e61be0065c18f1dae89d22f283ab
Ribonney <57567251+Ribonney@users.noreply.github.com>
2021-06-28 17:55:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from distutils.util import strtobool as sb
 
 API_HASH = os.getenv("API_HASH")
 API_ID = int(os.getenv("API_ID"))
@@ -6,6 +7,6 @@ DB_URL = os.getenv("DATABASE_URL")
 HEROKU_API = os.getenv("HEROKU_API")
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 PY_SESSION = os.getenv("PYROGRAM_SESSION")
-TE_SESSION = os.getenv("TELETHON_SESSION")
 PREFIX = os.environ.get("PREFIX", ".")
 LOG_CHAT = int(os.getenv("LOG_CHAT"))
+PM_AUTO_BAN = os.getenv('PM_AUTO_BAN', 'False')
